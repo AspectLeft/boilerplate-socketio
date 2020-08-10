2b8a34ece8c3709b708f905646ed7b64073801fd
Glitch (ginger-peaceful-silica) <none>
2020-08-10 23:07:35 +0000

    Completed Challenge 🌅🚤 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,15 +1,35 @@
-$( document ).ready(function() {
-  
-  
-   
+const appendMessage = message => {
+  $("#messages").append($("<li>").html("<b>" + message + "</b>"));
+}
+
+$(document).ready(function() {
+  /*global io*/
+  var socket = io();
+  socket.on("user count", function(data) {
+    console.log(data);
+  });
+  socket.on("user", function(data) {
+    $("#num-users").text(data.currentUsers + " users online");
+    var message = data.name;
+    if (data.connected) {
+      message += " has joined the chat.";
+    } else {
+      message += " has left the chat.";
+    }
+    appendMessage(message);
+  });
+  socket.on('chat message', (data) => {
+    $('#messages').append($('<li>').text(data.name+': '+data.message));
+  })
+
   // Form submittion with new message in field with id 'm'
-  $('form').submit(function(){
-    var messageToSend = $('#m').val();
+  $("form").submit(function() {
+    var messageToSend = $("#m").val();
     //send message to server here?
-    $('#m').val('');
+    
+    socket.emit('chat message', messageToSend);
+    
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
-  
-  
-  
 });
--- server.js
+++ server.js
@@ -1,50 +1,85 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const auth = require("./app/auth.js");
+const routes = require("./app/routes.js");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const app = express();
+const http = require("http").Server(app);
+const sessionStore = new session.MemoryStore();
 
+const passportSocketIo = require("passport.socketio");
+
+const cors = require("cors");
+app.use(cors());
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  key: 'express.sid',
-  store: sessionStore,
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    key: "express.sid",
+    store: sessionStore
+  })
+);
 
+mongo.connect(
+  process.env.MONGO_URI,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      console.log("Database error: " + err);
+      return;
+    }
+    const db = client.db("test");
+    console.log("Successful database connection");
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) console.log('Database error: ' + err);
-  
     auth(app, db);
     routes(app, db);
-      
+
     http.listen(process.env.PORT || 3000);
 
-  
-    //start socket.io code  
+    //start socket.io code
+    const io = require("socket.io")(http);
+    var currentUsers = 0;
 
-  
+    io.use(passportSocketIo.authorize({
+        cookieParser: cookieParser,
+        key: "express.sid",
+        secret: process.env.SESSION_SECRET,
+        store: sessionStore
+      })
+    );
+
+    io.on("connection", socket => {
+      console.log("A user has connected");
+      console.log('user ' + socket.request.user.name + ' connected');
+      ++currentUsers;
+      io.emit('user', {name: socket.request.user.name, currentUsers, connected: true});
+
+      socket.on('chat message', (message) => {
+        io.emit('chat message', {name: socket.request.user.name, message: message})
+      })
+      
+      socket.on("disconnect", () => {
+        --currentUsers;
+        io.emit('user', {name: socket.request.user.name, currentUsers, connected: false});
+      });
+    });
 
     //end socket.io code
-  
-  
-});
+  }
+);
